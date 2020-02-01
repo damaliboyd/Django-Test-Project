575818cd011f42f98a9df8a79055c68429ffedf0
damaliboyd <damaliboyd@ymail.com>
2020-01-31 19:54:42 -0500

    children now show up in parent detail

--- project/app01/admin.py
+++ project/app01/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from app01.models import Parent
+from app01.models import Parent, Child
 
 admin.site.register(Parent)
+admin.site.register(Child)
--- project/app01/models.py
+++ project/app01/models.py
@@ -6,3 +6,11 @@ class Parent(models.Model):
 
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+class Child(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
